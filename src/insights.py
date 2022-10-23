0473ca05d63fb8008ea5da1d61d6767f18a90182
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-10-23 10:55:11 -0300

    feat: implement get unique job industries

--- src/insights.py
+++ src/insights.py
@@ -58,7 +58,11 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    jobs_list = read(path)
+
+    job_industries = {job["industry"] for job in jobs_list if job["industry"]}
+
+    return job_industries
 
 
 def filter_by_industry(jobs, industry):
