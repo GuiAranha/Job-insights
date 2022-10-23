6057715c2ffd03f517f5f0396801457f9e078ec6
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-10-23 10:55:39 -0300

    feat: implement filter by job industry

--- src/insights.py
+++ src/insights.py
@@ -80,7 +80,9 @@ def filter_by_industry(jobs, industry):
     list
         List of jobs with provided industry
     """
-    return []
+    filter_job_industry = [job for job in jobs if job["industry"] == industry]
+
+    return filter_job_industry
 
 
 def get_max_salary(path):
